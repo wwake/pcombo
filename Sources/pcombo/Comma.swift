43a7da6fcb0981a3f71597ae4f9d03bbbecc875e
Bill Wake <william.wake@acm.org>
2022-08-01 10:45:01 -0500

    <&& - comma keep left => array

--- Sources/pcombo/Comma.swift
+++ Sources/pcombo/Comma.swift
@@ -27,8 +27,33 @@ where P1.Input == P2.Input {
   }
 }
 
+public class CommaLeft<P1: Parser, P2: Parser> : Parser
+where P1.Input == P2.Input {
+  public typealias Input = P1.Input
+  public typealias Target = [P1.Target]
+
+  let parser1 : P1
+  let parser2 : P2
+
+  public init(_ parser1: P1, _ parser2: P2) {
+    self.parser1 = parser1
+    self.parser2 = parser2
+  }
+
+  public func parse(_ input: ArraySlice<Input>) -> ParseResult<Input, Target> {
+    let parser = parser1 <&> <*>(parser2 &> parser1)
+    return parser.parse(input)
+  }
+}
+
 infix operator <&&> : MultiplicationPrecedence
 
 public func <&&> <P1: Parser, P2: Parser>(parser1: P1, parser2: P2) -> Comma<P1, P2> {
   return Comma(parser1, parser2)
+}
+
+infix operator <&& : MultiplicationPrecedence
+
+public func <&& <P1: Parser, P2: Parser>(parser1: P1, parser2: P2) -> CommaLeft<P1, P2> {
+  return CommaLeft(parser1, parser2)
 }
--- Tests/pcomboTests/CommaTests.swift
+++ Tests/pcomboTests/CommaTests.swift
@@ -51,4 +51,40 @@ final class CommaTests: XCTestCase {
     XCTAssertEqual(position, 0)
     XCTAssertEqual(message, "Did not find expected value")
   }
+
+  public func testCommaLeftMatchingOneItem() {
+    let sat1 = satisfy {$0 == 1}
+    let sat2 = satisfy {$0 == 2}
+    let grammar = sat1 <&& sat2
+
+    let result = grammar.parse([1,3])
+
+    checkSuccess(result, [1], [3])
+  }
+
+  public func testCommaLeftMatchingMultipleItems() {
+    let sat1 = satisfy {$0 == 1}
+    let sat2 = satisfy {$0 == 2}
+    let grammar = sat1 <&& sat2
+
+    let result = grammar.parse([1,2,1,3])
+
+    checkSuccess(result, [1, 1], [3])
+  }
+
+  public func testCommaLeftFailsToMatchFirstItem() {
+    let sat1 = satisfy {$0 == 1}
+    let sat2 = satisfy {$0 == 2}
+    let grammar = sat1 <&& sat2
+
+    let result = grammar.parse([2,1,2,3])
+
+    guard case let .failure(position, message) = result else {
+      XCTFail("Result was \(result)")
+      return
+    }
+
+    XCTAssertEqual(position, 0)
+    XCTAssertEqual(message, "Did not find expected value")
+  }
 }
