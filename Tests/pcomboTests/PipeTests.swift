0060c951a51a8fb22fa376272e78ec2570fbf84c
Bill Wake <william.wake@acm.org>
2022-08-04 10:07:16 -0500

    Make Pipe test use operator

--- Tests/pcomboTests/PipeTests.swift
+++ Tests/pcomboTests/PipeTests.swift
@@ -9,24 +9,6 @@
 import XCTest
 
 final class PipeTests: XCTestCase {
-  func testPipeConvertIntToCharacter() {
-    let sat1 = satisfy<Int> { _ in true }
-    let pipe = pcombo.Pipe<satisfy<Int>, Character>(sat1, { Array("ABCDE")[$0] })
-
-    let result = pipe.parse([1, 0, 3])
-
-    result.checkSuccess("B", [0,3])
-  }
-
-  func testPipeOnFailedParserReturnsFailure() {
-    let sat1 = satisfy<Int>("the message") { _ in false }
-    let pipe = pcombo.Pipe<satisfy<Int>, Character>(sat1, { Array("ABCDE")[$0] })
-
-    let result = pipe.parse([1, 0, 3])
-
-    result.checkFailure(.failure(0, "the message"))
-  }
-
   func testPipeOperatorConvertIntToCharacter() {
     let sat1 = satisfy<Int> { _ in true }
     let pipe = sat1 |> { Array("ABCDE")[$0] }
@@ -35,4 +17,14 @@ final class PipeTests: XCTestCase {
 
     result.checkSuccess("B", [0,3])
   }
+
+  func testPipeOnFailedParserReturnsFailure() {
+    let sat1 = satisfy<Int>("the message") { _ in false }
+    let pipe = sat1 |> { Array("ABCDE")[$0] }
+
+    let result = pipe.parse([1, 0, 3])
+
+    result.checkFailure(.failure(0, "the message"))
+  }
+
 }
