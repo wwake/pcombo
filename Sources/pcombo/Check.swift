03baf69b1e53805590f89456185d36bf4d55a348
Bill Wake <william.wake@acm.org>
2022-08-22 09:34:21 -0500

    Added Check2 - |&> operator - had already implemented in the Bassi project

--- Sources/pcombo/Check.swift
+++ Sources/pcombo/Check.swift
@@ -7,6 +7,38 @@
 
 import Foundation
 
+public class Check2<P: Parser, Target2> : Parser {
+  public typealias Input = P.Input
+  public typealias Target = Target2
+
+  let parser : P
+  let checker: (P.Target, ArraySlice<P.Input>)
+  -> ParseResult<P.Input, Target2>
+
+  init(_ parser: P, _ checker: @escaping (P.Target, ArraySlice<P.Input>) -> ParseResult<P.Input, Target2>) {
+    self.parser = parser
+    self.checker = checker
+  }
+
+  public func parse(_ input: ArraySlice<P.Input>) -> ParseResult<P.Input, Target2> {
+    let parse = parser.parse(input)
+
+    switch parse {
+    case .failure(let position, let message):
+      return .failure(position, message)
+
+    case .success(let target, let remaining):
+      return checker(target, remaining)
+    }
+  }
+}
+
+infix operator |&> : MultiplicationPrecedence
+
+public func |&> <P: Parser, Target2>(p: P, fn: @escaping (P.Target, ArraySlice<P.Input>) -> ParseResult<P.Input, Target2>) -> Check2<P, Target2> {
+  return Check2(p, fn)
+}
+
 public class Check<P: Parser> : Parser {
   public typealias Input = P.Input
   public typealias Target = P.Target
