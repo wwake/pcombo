70cd7bea945913b23b61a260aa4cfbf6d407fb8d
Bill Wake <william.wake@acm.org>
2022-08-22 09:37:28 -0500

    Delete old Check function

--- Sources/pcombo/Check.swift
+++ Sources/pcombo/Check.swift
@@ -38,39 +38,3 @@ infix operator |&> : MultiplicationPrecedence
 public func |&> <P: Parser, Target2>(p: P, fn: @escaping (P.Target, ArraySlice<P.Input>) -> ParseResult<P.Input, Target2>) -> Check2<P, Target2> {
   return Check2(p, fn)
 }
-
-public class Check<P: Parser> : Parser {
-  public typealias Input = P.Input
-  public typealias Target = P.Target
-
-  let parser : P
-  let checker: (Target) -> (Int, String)?
-
-  init(_ parser: P, _ checker: @escaping (Target) -> (Int, String)?) {
-    self.parser = parser
-    self.checker = checker
-  }
-
-  public func parse(_ input: ArraySlice<P.Input>) -> ParseResult<P.Input, P.Target> {
-    let parse = parser.parse(input)
-
-    switch parse {
-    case .failure:
-      return parse
-
-    case .success(let target, _):
-      let check = checker(target)
-      if let (location, message) = check {
-        return .failure(location, message)
-      } else {
-        return parse
-      }
-    }
-  }
-}
-
-infix operator <&| : MultiplicationPrecedence
-
-public func <&| <P: Parser>(p: P, fn: @escaping (P.Target) -> (Int, String)?) -> Check<P> {
-  return Check(p, fn)
-}
